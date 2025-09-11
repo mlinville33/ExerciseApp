f47a8de3a2e14fdbdfe3b001aa3cb7dc335e91cf
mlinville33 <96164133+mlinville33@users.noreply.github.com>
2025-09-11 15:04:40 -0600

    Update index.py

--- backendAPI/src/index.py
+++ backendAPI/src/index.py
@@ -1,18 +1,20 @@
-from fastapi import FastAPI, Query
-from src.selector.randomizer import randomizer, categories
+# from fastapi import FastAPI, Query
+# from src.selector.randomizer import randomizer, categories
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get('/')
-def root():
-    data = randomizer()
-    return data
+# @app.get('/')
+# def root():
+#     data = randomizer()
+#     return data
 
-@app.get('/health')
-def health_check():
-    return {'status': '200'}
+# @app.get('/health')
+# def health_check():
+#     return {'status': '200'}
 
-@app.get('/categories')
-def get_categories(search: str = Query(default=None, description='Returns the specified catgory')):
-    data = categories(search)
-    return data
+# @app.get('/categories')
+# def get_categories(search: str = Query(default=None, description='Returns the specified catgory')):
+#     data = categories(search)
+#     return data
+
+
